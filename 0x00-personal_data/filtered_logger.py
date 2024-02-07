78c7acbb4e2a7c865e64e2b1b3f701b1acaf0c3e
jerry nabango <jerrynabango9@gmail.com>
2024-02-07 19:28:16 +0300

    Filtered log

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -1,8 +1,10 @@
 #!/usr/bin/env python3
-"""log message obfuscated"""
+"""
+Defines a logger with custom log formatter
+"""
 import os
-import logging
 import re
+import logging
 from typing import List, Tuple
 
 import mysql.connector
@@ -15,7 +17,9 @@ def filter_datum(
     fields: List[str], redaction: str,
     message: str, separator: str
 ) -> str:
-    """Filter"""
+    """
+    Filters message by replacing each value in fields with redaction
+    """
     for key in fields:
         pattern = r'({0}=)[^{1}]*({1})'.format(key, separator)
         message = re.sub(pattern, r'\1{}\2'.format(redaction), message)
@@ -23,24 +27,31 @@ def filter_datum(
 
 
 class RedactingFormatter(logging.Formatter):
-    """Redacting Formatter class"""
+    """ Redacting Formatter class
+    """
     REDACTION = "***"
     FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
     SEPARATOR = ";"
 
     def __init__(self, fields: List[str]):
-        """Initialization"""
+        """
+        Instantiation method, sets fields for each instance
+        """
         super(RedactingFormatter, self).__init__(self.FORMAT)
         self.fields = fields
 
     def format(self, record: logging.LogRecord) -> str:
-        """Log Formatter"""
+        """
+        Formats the LogRecord instance
+        """
         log = super(RedactingFormatter, self).format(record=record)
         return filter_datum(self.fields, self.REDACTION, log, self.SEPARATOR)
 
 
 def get_logger() -> logging.Logger:
-    """Logs configuration"""
+    """
+    Creates and configures a logger
+    """
     logger = logging.getLogger('user_data')
     handler = logging.StreamHandler()
     handler.setFormatter(RedactingFormatter(fields=PII_FIELDS))
@@ -51,7 +62,9 @@ def get_logger() -> logging.Logger:
 
 
 def get_db() -> mysql.connector.connection.MySQLConnection:
-    """DB Connection"""
+    """
+    Connects to a mysql database
+    """
     connector = mysql.connector.connect(
         host=os.getenv('PERSONAL_DATA_DB_HOST'),
         database=os.getenv('PERSONAL_DATA_DB_NAME'),
@@ -62,7 +75,9 @@ def get_db() -> mysql.connector.connection.MySQLConnection:
 
 
 def main() -> None:
-    """Users List"""
+    """
+    Log database users
+    """
     db = get_db()
     logger = get_logger()
     cursor = db.cursor()

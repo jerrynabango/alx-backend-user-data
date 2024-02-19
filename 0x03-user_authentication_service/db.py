4ccd70146d750a3092c18a568d79d8d22a506c6b
Jerry Nabango <jerrynabango9@gmail.com>
2024-02-19 18:18:48 +0300

    Create ,Find & Update User

---
+++ 0x03-user_authentication_service/db.py
@@ -0,0 +1,58 @@
+#!/usr/bin/env python3
+"""create user"""
+
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm.session import Session
+from sqlalchemy.orm.exc import NoResultFound
+from sqlalchemy.exc import InvalidRequestError
+
+from user import Base, User
+
+
+class DatabaseManager:
+    """Database Management"""
+
+    def __init__(self) -> None:
+        """Initialize DB Connection"""
+        self._engine = create_engine("sqlite:///a.db", echo=False)
+        Base.metadata.drop_all(self._engine)
+        Base.metadata.create_all(self._engine)
+        self.__session = None
+
+    @property
+    def _session(self) -> Session:
+        """Get Session"""
+        if self.__session is None:
+            DBSession = sessionmaker(bind=self._engine)
+            self.__session = DBSession()
+        return self.__session
+
+    def add_user(self, email: str, hashed_password: str) -> User:
+        """Add a New User to the DB"""
+        user = User(email=email, hashed_password=hashed_password)
+        self._session.add(user)
+        self._session.commit()
+        return user
+
+    def find_user_by(self, **kwargs) -> User:
+        """Find User Based on Given Attributes"""
+        for key in kwargs:
+            if not hasattr(User, key):
+                raise InvalidRequestError
+        user = self._session.query(User).filter_by(**kwargs).first()
+        if user is None:
+            raise NoResultFound
+        return user
+
+    def update_user(self, user_id: int, **kwargs) -> None:
+        """Update an User's Information"""
+        user = self.find_user_by_attributes(id=user_id)
+        for key, value in kwargs.items():
+            if hasattr(user, key):
+                setattr(user, key, value)
+            else:
+                raise ValueError
+        self.__session.add(user)
+        self._session.commit()
